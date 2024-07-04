b18e5dc0f433d5447571c65cc7b45c3eeff881c0
alfiansyah <alfian.official.mail@gmail.com>
2024-07-04 13:12:33 +0700

    Initial Commit

---
+++ TheMovieDB/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TheMovieDB
+//
+//  Created by alfian on 04/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by alfian on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
