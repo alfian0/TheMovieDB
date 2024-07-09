69da2ddf209eaca82558e114373a76fd860f0071
alfiansyah <alfian.official.mail@gmail.com>
2024-07-09 19:30:56 +0700

    Give Info

--- TheMovieDB/Core/Utils/Component/SearchBar.swift
+++ TheMovieDB/Core/Utils/Component/SearchBar.swift
@@ -12,28 +12,29 @@ struct SearchBar: UIViewRepresentable {
     @Binding var text: String
 
     class Coordinator: NSObject, UISearchBarDelegate {
-        @Binding var text: String
+      @Binding var text: String
 
-        init(text: Binding<String>) {
-            _text = text
-        }
+      init(text: Binding<String>) {
+          _text = text
+      }
 
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            text = searchText
-        }
+      func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+          text = searchText
+      }
     }
 
     func makeCoordinator() -> Coordinator {
-        return Coordinator(text: $text)
+      return Coordinator(text: $text)
     }
 
     func makeUIView(context: Context) -> UISearchBar {
-        let searchBar = UISearchBar()
-        searchBar.delegate = context.coordinator
-        return searchBar
+      let searchBar = UISearchBar()
+      searchBar.delegate = context.coordinator
+      searchBar.placeholder = "Input at least 4 character"
+      return searchBar
     }
 
     func updateUIView(_ uiView: UISearchBar, context: Context) {
-        uiView.text = text
+      uiView.text = text
     }
 }
